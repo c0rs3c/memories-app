24fd1033aba4a505d0cfc4afc09053902ab8c4fc
Praween Prakash <praweenprakash@Praweens-MacBook-Pro.local>
2021-07-10 15:03:15 +0530

    Basic server and client working

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+export const PORT = process.env.PORT || 5000
+
+export const MONGOOSE_URI = 'mongodb+srv://praween:india@cluster0.l56bv.mongodb.net/memories?retryWrites=true&w=majority'
+
+// module.exports({PORT,MONGOOSE_URI})
---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+// import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+import * as config from './config/config.js'
+
+const app = express()
+
+app.use(cors())
+
+mongoose.connect(config.MONGOOSE_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(config.PORT, () => console.log(`Server running on ${config.PORT}`)))
+    .catch(error => console.error(error))
+
